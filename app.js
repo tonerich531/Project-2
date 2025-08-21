4d981ee46bac360302137370b54763765e720f25
tonerich531 <tonerich531@yahoo.com>
2025-08-21 09:00:50 -0700

    fifth push

--- app.js
+++ app.js
@@ -1,40 +1,67 @@
 // console.log(fetch("http://www.omdbapi.com/?i=tt3896198&apikey=fe9afa06&s=gangster"))
-const songYear = document.querySelector(".year");
-const movieWrapper = document.querySelector(".movies");
-const movieInfo = [] 
 
- movieWrapper.innerHTML = 
-                `<div class="movie">
-                        <figure>
-                            <img class="movie__poster--img" src="${movieInfo.Search['1'].Poster}" alt="">
-                        </figure>
-                        <div class="movie__title">
-                            <span class="text__purple">"${movieInfo.Search[1].Title}"</span>
-                        </div>
-                        <div class="show__type">${movieInfo.Search[1].Type}</div>
-                        <div class="show__year">${movieInfo.Search[1].Year}</div>
-                        <div class="imdb__id">${movieInfo.Search[1].imdbid}</div>                        
-                    </div>                                                        
-                </div>`;
+const moviesWrapper = document.querySelector('.movies');
+const searchName = document.querySelector('.search__name');
+ 
 
-// 1. Then
+function searchChange(event) {
+    renderMovies(event.target.value);
+    searchName.innerHTML = event.target.value;
+}
 
-// fetch("http://www.omdbapi.com/?i=tt3896198&apikey=fe9afa06&s=gangster").then((respnse) => {
-    // return respnse.json()        
-    // }).then(data => {
-        // console.log(data);
-        
-    // })
+let currentMovies = [];
 
-    // 2. Async/Await
-    async function main() {
-       const response =  await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=fe9afa06&s=gangster")
-       const data = await response.json();      
-       songYear.innerHTML = data.Search[0].Year;
-        const getMovies = data;
-        movieInfo.push(getMovies);
-        console.log(getMovies);       
-               
+
+
+// Fetch movies from the OMDB API based on the search term
+
+
+async function renderMovies(searchTerm) {
+    console.log(searchTerm)
+    const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=fe9afa06&s=${searchTerm}`)
+    const data = await response.json();
+    currentMovies = data.Search;
+    displayMovies(currentMovies);
+}
+
+// Display movies in the DOM
+
+
+function displayMovies(movieList) {
+    moviesWrapper.innerHTML = movieList
+    .slice(0, 6)
+    .map(movie => {
+        return `
+            <div class="movie">
+                <figure>
+                    <img class="movie__poster--img" src="${movie.Poster}" alt="">
+                </figure>
+                <h2>${movie.Title}</h2>
+                <h4>${movie.Year}</h4>
+                <button class="learn__more--btn">Learn More</button>
+            </div>
+            `
+        }).join("");
+}
+
+// Sort functionality
+function sortChange(event) {
+    const sortOption = event.target.value
+
+    let sortedMovies = [...currentMovies];
+    
+    if (sortOption === "newest") {
+        sortedMovies.sort((a, b) => b.Year - a.Year);
     }
 
-    main()
+    else if (sortOption === "oldest") {
+        sortedMovies.sort((a, b) => a.Year - b.Year);
+    }
+
+    displayMovies(sortedMovies);
+
+}
+    
+
+
+
